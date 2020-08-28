619190ffc852bfd6b0aaddd956582939af61e2c5
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-28 19:36:05 +0200

    Display weather info, event handlers

--- app/js/app.js
+++ app/js/app.js
@@ -76,3 +76,116 @@ class WEATHER {
     }
   }
 }
+
+// Display weather info
+class CITY {
+  constructor(json) {
+    this.name = json.city.name;
+    this.todayTemp = ConvertKelvinToCelcius(json.list[0].main.temp);
+    this.todayPress = json.list[0].main.pressure;
+    this.todayHuma = json.list[0].main.humidity;
+    this.todayWind = json.list[0].wind.speed;
+    this.todayIco = json.list[0].weather[0].main;
+    this.predWeather = [];
+    for (let i = 1; i <= 5; i++) {
+      const tempObj = {
+        hour: GetHourOfDay(json.list[i].dt_txt),
+        icon: json.list[i].weather[0].main,
+        temperature: ConvertKelvinToCelcius(json.list[i].main.temp),
+      };
+      this.predWeather.push(tempObj);
+    }
+  }
+
+  prepareBox = (DOM) => {
+    DOM.querySelectorAll(".city__name")[0].innerText = this.name;
+    DOM.querySelectorAll(".temperature__value")[0].innerText = this.todayTemp;
+    DOM.querySelectorAll(".pressure__value")[0].innerText =
+      this.todayPress + " hPa";
+    DOM.querySelectorAll(".humidity__value")[0].innerText =
+      this.todayHuma + "%";
+    DOM.querySelectorAll(".wind-speed__value")[0].innerText =
+      this.todayWind + " m/s";
+
+    DOM.querySelectorAll(".weather__forecast")[0].innerText = "";
+
+    this.predWeather.forEach((prediction) => {
+      const li = document.createElement("LI");
+
+      const citySpan = document.createElement("SPAN");
+      citySpan.classList.add("day");
+      citySpan.innerText = prediction.hour;
+
+      const img = document.createElement("IMG");
+      img.src = "images/icons/clear-day.svg";
+
+      const temperatureSpan = document.createElement("SPAN");
+      const valueSpan = document.createElement("SPAN");
+      valueSpan.innerText = prediction.temperature;
+      temperatureSpan.appendChild(valueSpan);
+
+      li.appendChild(citySpan);
+      li.appendChild(img);
+      li.appendChild(temperatureSpan);
+
+      DOM.querySelectorAll(".weather__forecast")[0].appendChild(li);
+    });
+  };
+}
+
+const main = async () => {
+  // User location info
+  const [lat, lon] = await new IP().Get();
+
+  // Weather info from user location
+  const weatherInfo = await new WEATHER().GetByLatLon(lat, lon);
+
+  // Prepare box
+  let firstBox = new CITY(weatherInfo);
+  firstBox.prepareBox($weatherBox);
+};
+
+main();
+
+// HANDLERS
+
+// event listeners handling
+$addButton.addEventListener("click", () => {
+  $searchBox.classList.toggle("d-none");
+});
+
+// search city handling
+$searchForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const cityName = formData.get("search");
+
+  // form clear
+  $searchForm.reset();
+
+  // loader
+  $body.classList.add("loading");
+  $searchBox.classList.toggle("d-none");
+
+  // get data from API
+  const wheatherInfo = await new WEATHER().GetByCity(cityName);
+  let newBox = new CITY(wheatherInfo);
+  console.log(newBox);
+
+  // new weather box
+  const newDOMBox = $weatherBox.cloneNode(true);
+  $weatherContainer.appendChild(newDOMBox);
+
+  // inserting data to weather box
+  newBox.prepareBox(newDOMBox);
+
+  // refresh references to buttons
+  $closeButtons = document.querySelectorAll(".btn--close");
+});
+
+// close weather box handling
+$closeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.parentNode.classList.add("d-none");
+  });
+});
