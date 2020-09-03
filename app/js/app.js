6062dd98c4443aa63194d72a7d7edddceccb5709
Michał Pielarz <pielarz.michal@gmail.com>
2020-09-03 03:01:38 +0200

    All nessesairy selectors added

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,26 @@
 import WEATHER_API_KEY from "./api_keys";
 
+// Components selectors
 const $body = document.querySelector("body");
 const $btnClose = document.querySelector(".btn-remove-module");
 const $btnFormClose = document.querySelector(".btn-add-module-form");
-const btnShowForm = document.querySelector("#add-city");
-const addForm = document.querySelector(".module__form");
-const cityNameInput = document.querySelector("#search");
+const $btnShowForm = document.querySelector("#add-city");
+const $addForm = document.querySelector(".module__form");
+const $cityNameInput = document.querySelector("#search");
+
+// Weather main module selectors
+const $weatherModule = document.querySelector(".module__weather");
+const $cityName = document.querySelector(".city__name");
+const $temperature = document.querySelector(".temperature__value");
+
+// Weather details module selectors
+const $pressure = document.querySelector(".pressure__value");
+const $humidity = document.querySelector(".humidity__value");
+const $windSpeed = document.querySelector(".wind-speed__value");
+const $weatherIcon = document.querySelector(".weather__icon").firstChild;
+
+// Weather forecast module selectors
+const $daysContent = document.querySelectorAll(".day");
+
+// Get user location
+let userLocation = async () => {};
